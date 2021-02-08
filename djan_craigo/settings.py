9b71f98edbc37b6da7f25d135aad6af16a201544
Yunhan <elitetai2006@gmail.com>
2021-02-08 22:22:12 +0800

    added import django_heroku

--- djan_craigo/settings.py
+++ djan_craigo/settings.py
@@ -13,9 +13,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 from dotenv import load_dotenv
 load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
